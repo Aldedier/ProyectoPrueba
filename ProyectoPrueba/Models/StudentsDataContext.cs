c7ff08b8f6a3f077d00054012585067abc804c9e
OSCAR ANDRES CESPEDES DIAZ <oscar.cespedes@correo.policia.gov.co>
2018-07-16 12:01:48 -0500

    Add project files.

---
+++ ProyectoPrueba/Models/StudentsDataContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoPrueba.Models
+{
+    public class StudentsDataContext : DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source = (localdb)\.; Initial Catalog = StudentsDB; Integrated Security = true "
+                );
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+
+    public class Student
+    {
+        [DatabaseGenerated(
+            DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Edad { get; set; }
+    }
+
+}
